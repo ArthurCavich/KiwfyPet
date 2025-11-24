8201074a6d2da1b3aa578428be6616f7ce0425be
Arthur Santana Cavichioli <arthurcavic@gmail.com>
2025-11-24 15:39:19 -0300

    Criado o projeto Spring Boot
    Utilizado o comando >Spring Initializr para criação do projeto Full-Strack. Usaremos a versão 21 do Java. As dependências são: Spring Web, JPA, H2, Lombok, Thymeleaf.

---
+++ kiwfy-pet/src/main/java/mvc2025/kiwfy_pet/KiwfyPetApplication.java
@@ -0,0 +1,13 @@
+package mvc2025.kiwfy_pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiwfyPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiwfyPetApplication.class, args);
+	}
+
+}
---
+++ kiwfy-pet/src/test/java/mvc2025/kiwfy_pet/KiwfyPetApplicationTests.java
@@ -0,0 +1,13 @@
+package mvc2025.kiwfy_pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiwfyPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
